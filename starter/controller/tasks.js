b19d5d65e750dea13d3c9fbd8c5f1059dcf8ac26
Bhavya1820 <bhavyasoni1820@gmail.com>
2024-11-25 11:22:19 +0530

    Learn Postman Testing

--- starter/controller/tasks.js
+++ starter/controller/tasks.js
@@ -1,7 +1,27 @@
 const getAllTasks = (req, res) => {
-  res.send('all items')
+  res.send('get all tasks')
+}
+
+const createTasks = (req, res) => {
+  res.send('create task')
+}
+
+const getTasks = (req, res) => {
+  res.send('get single task')
+}
+
+const updateTasks = (req, res) => {
+  res.send('update tasks')
+}
+
+const deleteTasks = (req, res) => {
+  res.send('delete tasks')
 }
 
 module.exports = {
   getAllTasks,
+  createTasks,
+  getTasks,
+  updateTasks,
+  deleteTasks,
 }
--- starter/routes/task.js
+++ starter/routes/task.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
-const {getAllTasks} = require('../controller/tasks')
+const {getAllTasks, createTasks, getTasks, updateTasks, deleteTasks} = require('../controller/tasks')
 
-router.route('/').get(getAllTasks)
+router.route('/').get(getAllTasks).post(createTasks);
+
+router.route('/:id').get(getTasks).patch(updateTasks).delete(deleteTasks);
+
 
 module.exports = router
