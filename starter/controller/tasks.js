9e0daae74c6717f3cff1ad35e4501928a037d0ba
Bhavya1820 <bhavyasoni1820@gmail.com>
2024-11-29 17:09:07 +0530

    Project Complete

--- starter/controller/tasks.js
+++ starter/controller/tasks.js
@@ -44,9 +44,42 @@ const deleteTasks = async (req, res) => {
   }
 };
 
-const updateTasks = (req, res) => {
-  res.send("update tasks");
+const updateTasks = async (req, res) => {
+  try {
+    const {id: taskID} = req.params;
+    const task = await Task.findOneAndUpdate({_id: taskID}, req.body, {
+      new: true,
+      runValidators: true,
+    })
+
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id : ${taskID}` });
+    }
+    
+    res.status(200).json({task})
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
+
+  // const editTask = async (req, res) => {
+  //   try {
+  //     const {id: taskID} = req.params;
+  //     const task = await Task.findOneAndUpdate({_id: taskID}, req.body, {
+  //       new: true,
+  //       runValidators: true,
+  //       overwrite: true,
+  //     })
+  
+  //     if (!task) {
+  //       return res.status(404).json({ msg: `No task with id : ${taskID}` });
+  //     }
+      
+  //     res.status(200).json({task})
+  //   } catch (error) {
+  //     res.status(500).json({ msg: error });
+  //   }
+  // };
 
 module.exports = {
   getAllTasks,
